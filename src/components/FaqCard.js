eafd1f430b2ad0fe5fda39129b3263ddb8b39a36
Jen Speak <jspeak6@gmail.com>
2020-12-30 13:12:56 -0800

    added styles for mobile images

--- src/components/FaqCard.js
+++ src/components/FaqCard.js
@@ -5,7 +5,9 @@ import Accordion from './Accordion';
 const FaqCard = (props) => {
     return (
         <section className="faq">
-            <div className="faq__image">
+            <div className="faq__illustrations">
+                <div className="faq__image"></div>
+                <div className="faq__bg"></div>
             </div>
             <Accordion faqs={props.faqs} />
         </section>
